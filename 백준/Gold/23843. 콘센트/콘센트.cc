c6a7f2467a000c234be0f997bd2b481271ceead1
jooh__0826 <110390562+parkjoohyung0826@users.noreply.github.com>
2025-01-22 12:24:31 +0900

    [Gold V] Title: 콘센트, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/23843. 콘센트/콘센트.cc
+++ 백준/Gold/23843. 콘센트/콘센트.cc
@@ -6,33 +6,47 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-
-    int N, M;
+    
+    int N, M; 
     cin >> N >> M;
-
-    vector<int> v(N);
+    
+    vector<int> v;
     for (int i = 0; i < N; i++) {
-        cin >> v[i];
+        int num;
+        cin >> num;
+        v.push_back(num);
+    };
+    
+    sort(v.begin(), v.end());
+    
+    // int p = N / (M);
+    vector<int> vv(M, 0);
+    
+    // for (int i = 0; i < M; i++) {
+    //     // int q = p*i;
+    //     // vv.push_back(v[q]);
+    //     // v[q] = 0;
+    //     vv[i] = 0;
+    // }
+    
+    priority_queue<int> PQ;
+    for(int i = 0; i < N; i++) {
+        PQ.push(v[i]);
+        // cout << v[i] << '\n';
     }
-
-    priority_queue<int, vector<int>, greater<int>> PQ;
-
-    for (int i = 0; i < M; i++) {
-        PQ.push(0); 
-    }
-
-    sort(v.rbegin(), v.rend()); 
-
-    for (int time : v) {
-        int earliest = PQ.top(); 
+    
+    // int S = N - M;
+    for (int i = 0; i < N; i++) {
+        
+        int num = PQ.top();
+        // cout << num << '\n';
         PQ.pop();
-        PQ.push(earliest + time); 
+        vv[0] += num;
+        sort(vv.begin(), vv.end());
     }
-
-    while (PQ.size() > 1) {
-        PQ.pop(); 
-    }
-    cout << PQ.top() << '\n'; 
-
+    
+    // for (auto i : vv) cout << i << " ";
+    cout << vv[M-1];
+    
     return 0;
 }
