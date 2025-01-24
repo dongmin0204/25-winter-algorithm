09cda4558670c2d0f2edadf173fa1f53b0332c62
DONGMIN_BAEK <nm2205@dgu.ac.kr>
2025-01-25 00:08:34 +0900

    [Silver III] Title: 시리얼 번호, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1431. 시리얼 번호/시리얼 번호.cc
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.cc
@@ -1,10 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 시리얼 번호의 숫자 합을 계산하는 함수
-int sum_of_digits(const string& s) {
+int sum_str(const string& s) {
     int sum = 0;
-    for(char c : s){
+    for(auto c : s){
         if(isdigit(c)){
             sum += c - '0';
         }
@@ -12,37 +11,43 @@ int sum_of_digits(const string& s) {
     return sum;
 }
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    
+struct Cmp {
+    bool operator()(const string& value, const string& other)  const{
+        if(value.length() != other.length()){
+            return value.length() < other.length(); //조건 1
+        }
+        else{
+            int sum_value = sum_str(value);
+            int sum_other = sum_str(other);
+            if(sum_value != sum_other){
+                return sum_value < sum_other;
+            }
+            else{
+                return value < other;
+            }
+        }
+    }
+};
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    vector<string> serial;
     int n;
     cin >> n;
-    vector<string> serial(n);
-    for(int i = 0; i < n; ++i){
-        cin >> serial[i];
+    string input;
+
+    for(int i = 0; i < n; i++){
+        cin >> input;
+        serial.push_back(input);
     }
-    
-    // 사용자 정의 정렬 기준
-    sort(serial.begin(), serial.end(), [&](const string& a, const string& b) -> bool {
-        // 1. 길이가 짧은 것이 먼저
-        if(a.length() != b.length()){
-            return a.length() < b.length();
-        }
-        // 2. 숫자의 합이 작은 것이 먼저
-        int sum_a = sum_of_digits(a);
-        int sum_b = sum_of_digits(b);
-        if(sum_a != sum_b){
-            return sum_a < sum_b;
-        }
-        // 3. 사전 순으로 비교 (숫자가 알파벳보다 작음)
-        return a < b;
-    });
-    
-    // 정렬된 시리얼 번호 출력
-    for(const auto& s : serial){
-        cout << s << '\n';
+
+    sort(serial.begin(),serial.end(),Cmp()); //비교하는 객체로 정렬 기준 구현
+
+    for(const auto& it : serial){
+        cout << it << '\n';
     }
-    
+
     return 0;
 }
