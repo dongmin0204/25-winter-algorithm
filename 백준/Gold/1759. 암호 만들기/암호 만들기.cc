acdbcb5059267d4f38228d184c7ab399cbbacade
DONGMIN_BAEK <nm2205@dgu.ac.kr>
2025-02-17 23:42:01 +0900

    Update 암호 만들기.cc

--- 백준/Gold/1759. 암호 만들기/암호 만들기.cc
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.cc
@@ -1,55 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int L, C;
-vector<char> code;
-vector<string> ans;
+int l, c;
+vector<char> code;        // 입력받은 문자들
+vector<string> ans;       // 가능한 암호를 저장할 벡터
 
-bool isVowel(char ch) {
-    return (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u');
+inline bool checkVowel(char ch) {
+    return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
 }
 
-void backtrack(int idx, int picked, int vowelCount, int consCount, string cur){
-    // base case: L개 골랐을 때
-    if(picked == L){
-        if(vowelCount >= 1 && consCount >= 2){
-            ans.push_back(cur);
+/* 
+// str: 현재 인덳 
+// start: 다음에 탐색할 code의 인덱스
+// vowel: 지금까지 포함된 모음 수
+// cons: 지금까지 포함된 자음 수
+// picked: 지금까지 몇 개의 문자를 골랐는지
+*/
+void bt(const string &str, int start, int vowel, int cons, int picked){
+    // start -> 이제 끝 도달
+    if (picked == l) {
+        // 조건(모음 1개 이상, 자음 2개 이상) 확인
+        if (vowel >= 1 && cons >= 2) {
+            ans.push_back(str);
         }
         return;
     }
 
-    // idx부터 C-1까지 탐색
-    for(int i = idx; i < C; i++){
+    // start부터 c-1까지 탐색
+    for (int i = start; i < c; i++) {
         char ch = code[i];
-        if(isVowel(ch)) {
-            // 모음이면 vowelCount+1
-            backtrack(i + 1, picked + 1, vowelCount + 1, consCount, cur + ch);
+        if (checkVowel(ch)) {
+            // 모음
+            bt(str + ch, i + 1, vowel + 1, cons, picked + 1);
         } else {
-            // 자음이면 consCount+1
-            backtrack(i + 1, picked + 1, vowelCount, consCount + 1, cur + ch);
+            // 자음
+            bt(str + ch, i + 1, vowel, cons + 1, picked + 1);
         }
     }
 }
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    cin >> L >> C;
+    cin >> l >> c;
+    code.resize(c);
 
-    code.resize(C);
-    for(int i = 0; i < C; i++){
+    // 문자 c개를 공백 구분으로 입력받기
+    for(int i = 0; i < c; i++){
         cin >> code[i];
     }
 
-    // 사전 순으로 만들기 위해 미리 정렬
+    // 사전 순 출력을 위해 정렬
     sort(code.begin(), code.end());
 
-    // (다음에 볼 인덱스 0, 아직 0개 골랐음, 모음/자음=0, 현재 문자열="")
-    backtrack(0, 0, 0, 0, "");
+    // 백트래킹
+    bt("", 0, 0, 0, 0);
 
-    for(auto &s : ans){
+    // 결과 출력
+    for (auto &s : ans){
         cout << s << "\n";
     }
+
     return 0;
 }
