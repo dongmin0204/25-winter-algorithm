35396a7f64526a88d248c22ad2345824e4b1556d
DONGMIN_BAEK <nm2205@dgu.ac.kr>
2025-03-02 16:58:18 +0900

    [Gold II] Title: 피보나치 수 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.cc
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+static const long long MOD = 1000000007;
+
+//행렬
+struct Matrix {
+    long long m[2][2];
+};
+
+// 2x2 행렬 곱셈
+Matrix matMul(const Matrix &A, const Matrix &B) {
+    Matrix R;
+    R.m[0][0] = (A.m[0][0] * B.m[0][0] + A.m[0][1] * B.m[1][0]) % MOD;
+    R.m[0][1] = (A.m[0][0] * B.m[0][1] + A.m[0][1] * B.m[1][1]) % MOD;
+    R.m[1][0] = (A.m[1][0] * B.m[0][0] + A.m[1][1] * B.m[1][0]) % MOD;
+    R.m[1][1] = (A.m[1][0] * B.m[0][1] + A.m[1][1] * B.m[1][1]) % MOD;
+    return R;
+}
+
+// 2x2 행렬 
+Matrix matPow(Matrix base, long long exp) {
+    // 단위 행렬
+    Matrix result;
+    result.m[0][0] = 1; result.m[0][1] = 0;
+    result.m[1][0] = 0; result.m[1][1] = 1;
+    
+    while (exp > 0) {
+        if (exp & 1) {
+            result = matMul(result, base);
+        }
+        base = matMul(base, base);
+        exp >>= 1;
+    }
+    return result;
+}
+
+// 피보나치
+long long fib(long long n) {
+    // B = [[1, 1], [1, 0]]
+    Matrix B;
+    B.m[0][0] = 1; B.m[0][1] = 1;
+    B.m[1][0] = 1; B.m[1][1] = 0;
+
+    if (n == 0) {
+        return 0;
+    }
+    // B의 n승
+    Matrix M = matPow(B, n);
+    // M[0][1] = F(n)
+    return M.m[0][1] % MOD; 
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long n;
+    cin >> n;
+    cout << fib(n) << "\n";
+    return 0;
+}
